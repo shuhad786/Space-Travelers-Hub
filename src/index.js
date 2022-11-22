724106b2c213e0beb02b8f12abb27f2e20e02f62
Shuhad786 <shuhad.loofer@gmail.com>
2022-11-22 11:55:43 +0200

    Corrected linter errors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './Redux/configureStore';
 import App from './App';
+import store from './Redux/configureStore';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
