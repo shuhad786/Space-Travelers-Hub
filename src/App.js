7bb17cceebb7ac14868df15d675f45f1fe381709
Shuhad786 <shuhad.loofer@gmail.com>
2022-11-23 11:13:51 +0200

    Rendered the missions info from API and badges

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useDispatch } from 'react-redux';
 import Missions from './Components/Missions';
 import Profiles from './Components/Profiles';
 import Rockets from './Components/Rockets';
-
 import { displayRocket, reserveRocket, cancelReserve } from './redux/rockets/rockets';
+import { displayMission, joinMission, cancelMission } from './redux/missions/missions';
 import './App.css';
 
 function App() {
@@ -13,19 +13,30 @@ function App() {
 
   useEffect(() => {
     dispatch(displayRocket());
+    dispatch(displayMission());
   }, []);
+
   const reserveRocketClick = (rocket) => {
     dispatch(reserveRocket(rocket));
   };
+
   const cancelReserveClick = (rocket) => {
     dispatch(cancelReserve(rocket));
+  };
+
+  const joinMissionClick = (mission) => {
+    dispatch(joinMission(mission));
+  };
+
+  const cancelMissionClick = (mission) => {
+    dispatch(cancelMission(mission));
   };
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Rockets reserveRocketClick={reserveRocketClick} cancelReserveClick={cancelReserveClick} />} />
-        <Route path="/missions" element={<Missions />} />
+        <Route path="/missions" element={<Missions joinMissionClick={joinMissionClick} cancelMissionClick={cancelMissionClick} />} />
         <Route path="/my-profile" element={<Profiles />} />
       </Routes>
     </div>
